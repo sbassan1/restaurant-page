8fb246510fb350f48d30c45c19fb973ae96aa8c7
Santiago Bassani <santiagobassani2000@gmail.com>
2025-02-11 18:35:58 -0300

    set the title and skeleton of the structures

--- src/index.js
+++ src/index.js
@@ -2,6 +2,54 @@ import "./styles.css";
 
 console.log("the index.js script is working!");
 
+const content = document.getElementById("content");
+
+const homeBtn = document.getElementById("home");
+const menuBtn = document.getElementById("menu");
+const aboutBtn = document.getElementById("about");
 
 
 
+class mainTitle {
+
+    render() {
+        const titleDiv = document.createElement("div");
+        const mainTitle = document.createElement("h1");
+        const undertitle = document.createElement("p");
+        
+        titleDiv.className = "title";
+        mainTitle.textContent = "ARGENTINA STEAKHOUSE CLUB";
+        undertitle.textContent = "Just the way it is.";
+        
+        titleDiv.appendChild(mainTitle);
+        titleDiv.appendChild(undertitle);
+        content.appendChild(titleDiv);
+    }
+
+}
+
+const title = new mainTitle();
+title.render();
+
+class homeContent {
+
+    render() {
+
+    }
+
+}
+
+class menuContent {
+
+    render() {
+
+    }
+
+}
+
+class aboutContent {
+
+    render(){
+
+    }
+}
