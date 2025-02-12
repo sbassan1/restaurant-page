44029a6bab1991d40c7d58729d7ab524f8832874
Santiago Bassani <santiagobassani2000@gmail.com>
2025-02-12 17:39:02 -0300

    Made the homepage along with the styling

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import "./styles.css";
 
+
 console.log("the index.js script is working!");
 
 const content = document.getElementById("content");
@@ -15,7 +16,7 @@ class MainTitle {
         const undertitle = document.createElement("p");
         
         titleDiv.className = "title";
-        mainTitle.textContent = "Argentina Steakhouse Gentlemans Club";
+        mainTitle.textContent = "Patagonia Grill";
         undertitle.textContent = "Just the way it is.";
         
         titleDiv.appendChild(mainTitle);
@@ -29,11 +30,21 @@ class HomeContent {
         const homeDiv = document.createElement("div");
         homeDiv.className = "home-div";
 
-        const presentation_fieldset = document.createElement("fieldset");
+        const presentation_fieldset = document.createElement("fieldset");   
+        presentation_fieldset.className = "fieldset-div";
+
         const fieldset_title = document.createElement("legend");
         fieldset_title.textContent = "Who are we?";
 
+        const introd_text = document.createElement("h2");
+        introd_text.textContent = "We are argentinian chefs looking to bring some of our countries most amazing foods here";
+
+        const explanation_text = document.createElement("p");
+        explanation_text.textContent = "Patagonia Grill is more than just a steakhouse; it's a journey to the heart of Argentina. We're dedicated to showcasing the country's rich culinary heritage, using time-honored techniques and the finest ingredients. From the sizzle of the grill to the warmth of our hospitality, we invite you to experience the passion and tradition that define Argentinian cuisine.";
+
         presentation_fieldset.appendChild(fieldset_title);
+        presentation_fieldset.appendChild(introd_text);
+        presentation_fieldset.appendChild(explanation_text);
         
 
         const mission_div = document.createElement("div");
@@ -41,7 +52,7 @@ class HomeContent {
         const mission_title = document.createElement("h2");
         mission_title.textContent = "We want you to experience TRUE Argentinian Cuisine!";
         const mission_txt = document.createElement("p");
-        mission_txt.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor."
+        mission_txt.textContent = "Indulge in Argentina's finest cuts. We specialize in traditional parrilla grilling, offering succulent Asado (ribs), flavorful Vacio (flank steak), tender Bondiola (pork shoulder), and more. Each steak is expertly prepared to capture the authentic taste of Argentina.";
 
         mission_div.appendChild(mission_title);
         mission_div.appendChild(mission_txt);
